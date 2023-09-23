81c54d834db5c0720f11a3c32f5f7ac2c691bc36
Jorge Benjumea Burillo <me@intron014.com>
2023-09-23 18:33:17 +0200

    Generic dot

--- proyect1/src/main/java/I014/genericpunto.java
+++ proyect1/src/main/java/I014/genericpunto.java
@@ -3,11 +3,16 @@ package I014;
 
 public class genericpunto {
     private double[] coordenadas;
+    private COLOR color;
     public genericpunto(int dimensiones) {
         this.coordenadas = new double[dimensiones];
+        this.color=COLOR.ROJO;
     }
     public void setCoordenadas(double[] coordenadas) {
         this.coordenadas = coordenadas;
+    }
+    public enum COLOR {
+        ROJO, AZUL, AMARILLO;
     }
     public String toString() {
         String resultado = "(";
