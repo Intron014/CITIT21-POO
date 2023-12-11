ee13570b74b7a8810068d00a7d659728ffd373e7
Jorge Benjumea Burillo <me@intron014.com>
2023-12-11 18:07:39 +0100

    Multimapping

---
+++ src/main/java/MultiMap/App.java
@@ -0,0 +1,12 @@
+package MultiMap;
+
+import java.util.Map;
+
+public class App {
+    // public static void main(String[] args) {
+    //     Map<Integer, String> mm = new MultiMap<Integer, String>();
+    //     Pair p = new Pair();
+    //     p.setOne("Hola");
+    //     Integer msg = (Integer) p.getOne();
+    // }
+}
---
+++ src/main/java/MultiMap/MultiMap.java
@@ -0,0 +1,79 @@
+package MultiMap;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+
+public class MultiMap<K, V> implements Map<K, V> {
+
+    private Map<K, List<V>> objects;
+
+    public MultiMap(){
+        objects = new HashMap<>();
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public V put(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(@NotNull Map<? extends K, ? extends V> m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @NotNull
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public Collection<V> values() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+}
